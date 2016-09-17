ce3c897a2c2808787ef9abe511dfe488108cc011
Yuya Soneoka <ej.a9p96@gmail.com>
2016-09-17 11:58:00 +0900

    Initial Commit

---
+++ TVar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVar
+//
+//  Created by yuya on 2016/09/17.
+//  Copyright © 2016年 soneoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
