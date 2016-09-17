5fdd29b1b8f1d2d9c9c93072a55b930bc04c5faa
Yuya Soneoka <ej.a9p96@gmail.com>
2016-09-17 21:28:01 +0900

    Fix a choose view design

--- TVar/ChoosePhotoViewController.swift
+++ TVar/ChoosePhotoViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ChoosePhotoViewController: UIViewController {
 
+    @IBOutlet weak var videoImage: UIImageView!
+    @IBOutlet weak var photoFlag: UIImageView!
+    @IBOutlet weak var chooseBox: UIView!
+    @IBOutlet weak var selectImage: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(false, animated: false)
+        setDesign()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: Design
+    func setDesign() {
+        photoFlag.hidden = true
+        chooseBox.layer.cornerRadius = 5
+        videoImage.layer.cornerRadius = 5
+        chooseBox.layer.borderWidth = 1.0
+        chooseBox.layer.borderColor = UIColor.grayColor().CGColor
+        selectImage.layer.cornerRadius = 5
+    }
+    
+    // MARK: Button
+    @IBAction func cameraButtonPushed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func albumButtonPushed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
 }
